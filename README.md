c4367f33dda8a76d059ddb4943be695c930394a5
Valtteri Rehu <90039969+Valde00@users.noreply.github.com>
2026-01-30 23:16:19 +0200

    a

---
+++ README.md
@@ -0,0 +1,12 @@
+Compose:
+
+- With Compose, whenever the UI state changes, Compose recomposes automatically. 
+
+ViewModel:
+
+- ViewModel is better than remember, because it lasts through process changes and Composes, unlike remember.
+
+
+Showcase:
+
+https://youtu.be/paSYZ0gioYY
---
+++ app/src/main/java/com/example/viikko1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.viikko1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.viikko1.ui.theme.Viikko1Theme
+import com.example.viikko1.domain.HomeScreen
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Viikko1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    HomeScreen(modifier = Modifier.padding(innerPadding))
+                }
+            }
+        }
+    }
+}
